312d6d76814ef80d313b73e1c686dc79dcbbb497
SahanaKatta07 <sahanakatta712@gmail.com>
2024-07-29 18:23:41 +0530

    Added changes

---
+++ src/main/java/com/spoors/setting/MobileSqls.java
@@ -0,0 +1,25 @@
+package com.spoors.setting;
+
+public class MobileSqls {
+	
+	public static final String FORMS_SPECS_LIST_FILTERING_CRITERIAS = "CREATE TABLE forms_specs_list_filtering_criterias (json_data TEXT DEFAULT (null),processed INTEGER,autoId INTEGER)";
+	public static final String FORMS_SPECS_FORM_SPECS_VISIBILITY_MAP = "CREATE TABLE forms_specs_form_specs_visibility_map (json_data TEXT DEFAULT (null))";
+	public static final String FORMS_SPECS_FORM_SPEC_DATA_SOURCE  = "CREATE TABLE forms_specs_form_spec_data_source (json_data TEXT DEFAULT (null),processed INTEGER,autoId INTEGER)";
+	public static final String FORMS_SPECS_JOB_FORM_FIELD_SPEC_MAPPING  = "CREATE TABLE forms_specs_job_form_field_spec_mapping (json_data TEXT DEFAULT (null),processed INTEGER,autoId INTEGER)";
+	public static final String FORMS_SPECS_DATA_SOURCE_REQUEST_PARAMS ="CREATE TABLE forms_specs_data_source_request_params (json_data TEXT DEFAULT (null),processed INTEGER,autoId INTEGER)";
+	public static final String FORMS_SPECS_DATA_SOURCE_REQUEST_HEADERS = "CREATE TABLE forms_specs_data_source_request_headers (json_data TEXT DEFAULT (null),processed INTEGER,autoId INTEGER)";
+	public static final String FORMS_SPECS_DATA_SOURCE_RESPONSE_MAPPINGS = "CREATE TABLE forms_specs_data_source_response_mappings (json_data TEXT DEFAULT (null),processed INTEGER,autoId INTEGER)";
+	public static final String FORMS_SPECS_FIELDS = "CREATE TABLE forms_specs_fields (json_data TEXT DEFAULT (null),processed INTEGER,autoId INTEGER)";
+	public static final String FORMS_SPECS_SECTION_FIELDS = "CREATE TABLE forms_specs_section_fields (json_data TEXT DEFAULT (null),processed INTEGER,autoId INTEGER)";
+	public static final String FORMS_SPECS_FORM_SPECS = "CREATE TABLE forms_specs_form_specs (json_data TEXT DEFAULT (null),processed INTEGER,autoId INTEGER)";
+	public static final String FORMS_SPECS_PAGE_SPECS = "CREATE TABLE forms_specs_page_specs (json_data TEXT DEFAULT (null),processed INTEGER,autoId INTEGER)";
+	public static final String FORMS_SPECS_FIELD_VALID_VALUES = "CREATE TABLE forms_specs_field_valid_values (json_data TEXT DEFAULT (null),processed INTEGER,autoId INTEGER)";
+	public static final String FORMS_SPECS_SECTIONS = "CREATE TABLE forms_specs_sections (json_data TEXT DEFAULT (null),processed INTEGER,autoId INTEGER)";
+	public static final String FORMS_SPECS_SECTION_FIELD_VALID_VALUES = "CREATE TABLE forms_specs_section_field_valid_values (json_data TEXT DEFAULT (null),processed INTEGER,autoId INTEGER)";
+	public static final String FORMS_SPECS_VISIBILITY_DEPENDENCY_CRITERIAS = "CREATE TABLE forms_specs_visibility_dependency_criterias (json_data TEXT DEFAULT (null),processed INTEGER,autoId INTEGER)";
+	public static final String FORM_FIELDS_COLOR_DEPENDENCY_CRITERIAS = "CREATE TABLE form_fields_Color_dependency_criterias (json_data TEXT DEFAULT (null),processed INTEGER,autoId INTEGER)";
+	public static final String FORM_CLEAN_UP_RULES = "CREATE TABLE form_clean_up_rules (json_data TEXT DEFAULT (null),processed INTEGER,autoId INTEGER)";
+	public static final String FORMS_SPECS_CUSTOMER_AUTO_FILTERING_CRITERIAS = "CREATE TABLE forms_specs_customer_auto_filtering_criterias (json_data TEXT DEFAULT (null),processed INTEGER,autoId INTEGER)";
+
+
+}
